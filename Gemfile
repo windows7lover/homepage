9a9bccad71cc985f42f3b13337c7c05139aafeaa
windows7lover <damien.scieur@gmail.com>
2017-03-08 17:46:14 +0100

    added awesome ruby function for bibtex

--- Gemfile
+++ Gemfile
@@ -5,4 +5,4 @@ gem 'jekyll-scholar'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'sass-rails', '>= 3.2'
 gem 'kramdown'
-gem "pygments.rb"
+gem 'pygments.rb'
---
+++ _plugins/FindBibEntry.rb
@@ -0,0 +1,51 @@
+module Jekyll
+
+
+
+  module FindBibEntry
+		
+		BanStrings = ["paperpdf","abstract"]
+
+		non_delimiters = /[^(){}\[\]]*/
+		Paired = /\(#{non_delimiters}\)|\{#{non_delimiters}\}|\[#{non_delimiters}\]/
+		Delimiter = /[(){}\[\]]/
+		def balanced? s # check if s is ballanced between two brackets
+			s = s.dup
+			s.gsub!(Paired, "".freeze) while s =~ Paired
+			s !~ Delimiter
+		end
+
+    def find_bib_entry(filename, key)
+		# search a bib entry based on the key
+			entryfound = false;
+			s = "";
+			File.open(filename) do |f|
+				f.any? do |line|
+
+					if entryfound
+						if not balanced?(s) # if s in unbalanced in parenthesis or other, then we need to continue.
+							if not BanStrings.any? { |banstr| line.include? banstr }
+								s << line; # we check if the line contains a forbiden string, like paperpdf
+							end
+						else
+							break;  # we are done!
+						end
+					end
+
+					if (line.include?(key) && !entryfound)
+						entryfound = true #if the key is found, be append the line
+						s << line;
+						if balanced?(s)
+							
+						end
+					end
+					
+				end
+			end
+			s # return
+			return s
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::FindBibEntry)
